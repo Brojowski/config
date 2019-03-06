0c236f62555cf21cf2eb28511cf5cb88fccd1343
Brojowski <Brojowski@users.noreply.github.com>
2019-03-06 11:02:09 -0500

    Update iOS-Class-Navigation.user.js

--- iOS-Class-Navigation.user.js
+++ iOS-Class-Navigation.user.js
@@ -3,6 +3,7 @@
 // @version  1
 // @grant    none
 // @include https://web.cs.kent.edu/~whaverst/Edu/Courses/IOS/Notes/*
+// @updateURL https://raw.githubusercontent.com/Brojowski/custom-styles/master/iOS-Class-Navigation.user.js
 // ==/UserScript==
 
 var links = document.getElementsByTagName('a')
@@ -12,33 +13,33 @@ var next = null;
 var up = null;
 
 for (var i = 0; i < links.length; i++) {
-  switch (links[i].innerHTML) {
-    case 'prev':
-      prev = links[i];
-      break;
-    case 'next':
-      next = links[i];
-      break;
-    case 'up':
-      up = links[i];
-      break;
-  }
+    switch (links[i].innerHTML) {
+        case 'prev':
+            prev = links[i];
+            break;
+        case 'next':
+            next = links[i];
+            break;
+        case 'up':
+            up = links[i];
+            break;
+    }
 }
 
 if (!up || !next || !prev) {
-  console.error('keybindings broken');
+    console.error('keybindings broken');
 }
 
 document.onkeydown = function (event) {
-  switch (event.which) {
-    case 39: // Next
-      next.click();
-      break;
-    case 37: // Prev
-      prev.click();
-      break;
-    case 38: // Up
-      up.click();
-      break;
-  }
+    switch (event.which) {
+        case 39: // Next
+            next.click();
+            break;
+        case 37: // Prev
+            prev.click();
+            break;
+        case 38: // Up
+            up.click();
+            break;
+    }
 };
