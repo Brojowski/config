cf5d65db8fc9db289bcf4a3f3ffe59b902031b1a
Brojowski <Brojowski@users.noreply.github.com>
2019-03-06 10:58:06 -0500

    Create iOS-Class-Navigation.user.js

---
+++ iOS-Class-Navigation.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name     iOS KeyBindings
+// @version  1
+// @grant    none
+// @include https://web.cs.kent.edu/~whaverst/Edu/Courses/IOS/Notes/*
+// ==/UserScript==
+
+var links = document.getElementsByTagName('a')
+
+var prev = null;
+var next = null;
+var up = null;
+
+for (var i = 0; i < links.length; i++) {
+  switch (links[i].innerHTML) {
+    case 'prev':
+      prev = links[i];
+      break;
+    case 'next':
+      next = links[i];
+      break;
+    case 'up':
+      up = links[i];
+      break;
+  }
+}
+
+if (!up || !next || !prev) {
+  console.error('keybindings broken');
+}
+
+document.onkeydown = function (event) {
+  switch (event.which) {
+    case 39: // Next
+      next.click();
+      break;
+    case 37: // Prev
+      prev.click();
+      break;
+    case 38: // Up
+      up.click();
+      break;
+  }
+};
