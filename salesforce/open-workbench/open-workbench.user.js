a2219e2dbfa458f942ecea2777d79d0fb43ba43c
alex.gajowski <alex.gajowski@veeva.com>
2023-03-20 13:37:49 -0400

    Create Open Workbench Script

---
+++ salesforce/open-workbench/open-workbench.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Salesforce: Open Workbench
+// @namespace    github.com/Brojowski
+// @version      0.1
+// @description  Quickly switch between Salesforce Profiles without leaving the object view
+// @author       Brojowski
+// @match        https://*.salesforce.com/*
+// @match        https://*.force.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=force.com
+// @updateURL    https://raw.githubusercontent.com/Brojowski/config/master/salesforce/open-workbench/open-workbench.user.js
+// @downloadURL  https://raw.githubusercontent.com/Brojowski/config/master/salesforce/open-workbench/open-workbench.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
